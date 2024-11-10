cb441acb9547326702d7211e592d9b0bf1069d3b
Jasper <jasperris93@gmail.com>
2024-11-10 16:30:30 +0000

    setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="app",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=["pyspark"],
+    entry_points={
+        "console_scripts": ["app = app:main.main"],
+    },
+    author="",
+    author_email="",
+    description="",
+    url="",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
